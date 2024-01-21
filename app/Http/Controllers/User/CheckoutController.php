fed8a96efb03e2af693ee2072bf57fbeb35ba12f
peterpsd7 <peterpsd7@gmail.com>
2024-01-21 20:18:37 +0700

    integrasi landinpage, checkout, validasi

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\Checkout\Store;
 use App\Models\Camps;
 use App\Models\Checkout;
 use Illuminate\Http\Request;
@@ -21,8 +22,12 @@ class CheckoutController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create(Camps $camps)
+    public function create(Camps $camps, Request $request)
     {
+        if ($camps->isRegistered) {
+            $request->session()->flash('error', "You already registered on {$camps->title} camp.");
+            return redirect(route('dashboard'));
+        }
         return view('checkout.create', [
             'camps' => $camps,
         ]);
@@ -31,7 +36,7 @@ class CheckoutController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request, Camps $camps)
+    public function store(Store $request, Camps $camps)
     {
         //mapping request data
         $data = $request->all();
--- resources/views/checkout/create.blade.php
+++ resources/views/checkout/create.blade.php
@@ -33,15 +33,24 @@
                             @csrf
                             <div class="mb-4">
                                 <label for="exampleInputEmail1" class="form-label">Full Name</label>
-                                <input type="text" name="name" class="form-control" value="{{Auth::user()->name}}" />
+                                <input type="text" name="name" class="form-control {{$errors->has('name') ? 'is-invalid' : ''}}" value="{{Auth::user()->name}}" />
+                                @if ($errors->has('name'))
+                                <p class="text-danger">{{$errors->first('name')}}</p>
+                            @endif
                             </div>
                             <div class="mb-4">
                                 <label for="exampleInputEmail1" class="form-label">Email Address</label>
-                                <input type="email" name="email" class="form-control" value="{{Auth::user()->email}}"/>
+                                <input type="email" name="email" class="form-control {{$errors->has('email') ? 'is-invalid' : ''}}" value="{{Auth::user()->email}}"/>
+                                @if ($errors->has('email'))
+                                <p class="text-danger">{{$errors->first('email')}}</p>
+                            @endif
                             </div>
                             <div class="mb-4">
-                                <label for="exampleInputEmail1" class="form-label">Occupation</label>
-                                <input type="text" name="occupation" class="form-control" value="{{Auth::user()->occupation}}"/>
+                                <label class="form-label">Occupation</label>
+                                <input name="occupation" type="text" class="form-control {{$errors->has('occupation') ? 'is-invalid' : ''}}" value="{{old('occupation') ?: Auth::user()->occupation}}" />
+                                @if ($errors->has('occupation'))
+                                    <p class="text-danger">{{$errors->first('occupation')}}</p>
+                                @endif
                             </div>
                             <div class="mb-4">
                                 <label for="exampleInputEmail1" class="form-label">Card Number</label>
