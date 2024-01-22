19623101b21f04ddb5669cbac38a51ecc863064f
peterpsd7 <peterpsd7@gmail.com>
2024-01-22 14:39:15 +0700

    update validation

--- app/Http/Requests/User/Checkout/Store.php
+++ app/Http/Requests/User/Checkout/Store.php
@@ -30,6 +30,9 @@ class Store extends FormRequest
             'name' => 'required|string',
             'email' => 'required|email|unique:users,email,'.Auth::id().',id',
             'occupation' => 'required|string',
+            'card_number' => 'required|numeric|digits_between:8,16',
+            'exp' => 'required|date|date_format:Y-m|after_or_equal:'.$expiredValidation,
+            'cvc' => 'required|numeric|digits:3',
         ];
     }
 }
--- resources/views/checkout/create.blade.php
+++ resources/views/checkout/create.blade.php
@@ -1,82 +1,105 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="checkout">
-    <div class="container">
-        <div class="row text-center pb-70">
-            <div class="col-lg-12 col-12 header-wrap">
-                <p class="story">
-                    YOUR FUTURE CAREER
-                </p>
-                <h2 class="primary-header">
-                    Start Invest Today
-                </h2>
+    <section class="checkout">
+        <div class="container">
+            <div class="row text-center pb-70">
+                <div class="col-lg-12 col-12 header-wrap">
+                    <p class="story">
+                        YOUR FUTURE CAREER
+                    </p>
+                    <h2 class="primary-header">
+                        Start Invest Today
+                    </h2>
+                </div>
             </div>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-lg-9 col-12">
-                <div class="row">
-                    <div class="col-lg-5 col-12">
-                        <div class="item-bootcamp">
-                            <img src="{{asset('assets/images/item_bootcamp.png')}}" alt="" class="cover">
-                            <h1 class="package text-uppercase">
-                                {{$camps->title}}
-                            </h1>
-                            <p class="description">
-                                Bootcamp ini akan mengajak Anda untuk belajar penuh mulai dari pengenalan dasar sampai membangun sebuah projek asli
-                            </p>
+            <div class="row justify-content-center">
+                <div class="col-lg-9 col-12">
+                    <div class="row">
+                        <div class="col-lg-5 col-12">
+                            <div class="item-bootcamp">
+                                <img src="{{ asset('assets/images/item_bootcamp.png') }}" alt="" class="cover">
+                                <h1 class="package text-uppercase">
+                                    {{ $camps->title }}
+                                </h1>
+                                <p class="description">
+                                    Bootcamp ini akan mengajak Anda untuk belajar penuh mulai dari pengenalan dasar sampai
+                                    membangun sebuah projek asli
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-lg-1 col-12"></div>
-                    <div class="col-lg-6 col-12">
-                        <form action="{{route('checkout.store', $camps->id)}}" class="basic-form" method="POST">
-                            @csrf
-                            <div class="mb-4">
-                                <label for="exampleInputEmail1" class="form-label">Full Name</label>
-                                <input type="text" name="name" class="form-control {{$errors->has('name') ? 'is-invalid' : ''}}" value="{{Auth::user()->name}}" />
-                                @if ($errors->has('name'))
-                                <p class="text-danger">{{$errors->first('name')}}</p>
-                            @endif
-                            </div>
-                            <div class="mb-4">
-                                <label for="exampleInputEmail1" class="form-label">Email Address</label>
-                                <input type="email" name="email" class="form-control {{$errors->has('email') ? 'is-invalid' : ''}}" value="{{Auth::user()->email}}"/>
-                                @if ($errors->has('email'))
-                                <p class="text-danger">{{$errors->first('email')}}</p>
-                            @endif
-                            </div>
-                            <div class="mb-4">
-                                <label class="form-label">Occupation</label>
-                                <input name="occupation" type="text" class="form-control {{$errors->has('occupation') ? 'is-invalid' : ''}}" value="{{old('occupation') ?: Auth::user()->occupation}}" />
-                                @if ($errors->has('occupation'))
-                                    <p class="text-danger">{{$errors->first('occupation')}}</p>
-                                @endif
-                            </div>
-                            <div class="mb-4">
-                                <label for="exampleInputEmail1" class="form-label">Card Number</label>
-                                <input type="number" name="card_number" class="form-control" />
-                            </div>
-                            <div class="mb-5">
-                                <div class="row">
-                                    <div class="col-lg-6 col-12">
-                                        <label for="exampleInputEmail1" class="form-label">Expired</label>
-                                        <input type="date" name="exp" class="form-control" />
-                                    </div>
-                                    <div class="col-lg-6 col-12">
-                                        <label for="exampleInputEmail1" class="form-label">CVC</label>
-                                        <input type="number" name="cvc" class="form-control" maxlength="3"/>
+                        <div class="col-lg-1 col-12"></div>
+                        <div class="col-lg-6 col-12">
+                            <form action="{{ route('checkout.store', $camps->id) }}" class="basic-form" method="POST">
+                                @csrf
+                                <div class="mb-4">
+                                    <label for="exampleInputEmail1" class="form-label">Full Name</label>
+                                    <input type="text" name="name"
+                                        class="form-control {{ $errors->has('name') ? 'is-invalid' : '' }}"
+                                        value="{{ Auth::user()->name }}" required />
+                                    @if ($errors->has('name'))
+                                        <p class="text-danger">{{ $errors->first('name') }}</p>
+                                    @endif
+                                </div>
+                                <div class="mb-4">
+                                    <label for="exampleInputEmail1" class="form-label">Email Address</label>
+                                    <input type="email" name="email"
+                                        class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
+                                        value="{{ Auth::user()->email }}" required />
+                                    @if ($errors->has('email'))
+                                        <p class="text-danger">{{ $errors->first('email') }}</p>
+                                    @endif
+                                </div>
+                                <div class="mb-4">
+                                    <label class="form-label">Occupation</label>
+                                    <input name="occupation" type="text"
+                                        class="form-control {{ $errors->has('occupation') ? 'is-invalid' : '' }}"
+                                        value="{{ old('occupation') ?: Auth::user()->occupation }}" required />
+                                    @if ($errors->has('occupation'))
+                                        <p class="text-danger">{{ $errors->first('occupation') }}</p>
+                                    @endif
+                                </div>
+                                <div class="mb-4">
+                                    <label for="exampleInputEmail1" class="form-label">Card Number</label>
+                                    <input type="number" name="card_number"
+                                        class="form-control {{ $errors->has('card_number') ? 'is-invalid' : '' }}"
+                                        value="{{ old('card_number') ?: '' }}" required />
+                                    @if ($errors->has('card_number'))
+                                        <p class="text-danger">{{ $errors->first('card_number') }}</p>
+                                    @endif
+                                </div>
+                                <div class="mb-5">
+                                    <div class="row">
+                                        <div class="col-lg-6 col-12">
+                                            <label for="exampleInputEmail1" class="form-label">Expired</label>
+                                            <input type="month" name="exp"
+                                                class="form-control {{ $errors->has('exp') ? 'is-invalid' : '' }}"
+                                                value="{{ old('exp') ?: '' }}" required />
+                                            @if ($errors->has('exp'))
+                                                <p class="text-danger">{{ $errors->first('exp') }}</p>
+                                            @endif
+                                        </div>
+                                        <div class="col-lg-6 col-12">
+                                            <label for="exampleInputEmail1" class="form-label">CVC</label>
+                                            <input type="number" name="cvc"
+                                                class="form-control {{ $errors->has('cvc') ? 'is-invalid' : '' }}"
+                                                maxlength="3" value="{{ old('cvc') ?: '' }}" required />
+                                            @if ($errors->has('cvc'))
+                                                <p class="text-danger">{{ $errors->first('cvc') }}</p>
+                                            @endif
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <button type="submit" class="w-100 btn btn-primary">Pay Now</button>
-                            <p class="text-center subheader mt-4">
-                                <img src="{{asset('assets/images/ic_secure.svg')}}" alt=""> Your payment is secure and encrypted.
-                            </p>
-                        </form>
+                                <button type="submit" class="w-100 btn btn-primary">Pay Now</button>
+                                <p class="text-center subheader mt-4">
+                                    <img src="{{ asset('assets/images/ic_secure.svg') }}" alt=""> Your payment is
+                                    secure and encrypted.
+                                </p>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
 @endsection
